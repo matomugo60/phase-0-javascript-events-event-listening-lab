9da5ae7cf32b54d7ffd4bc7fc72cf1195a02c49d
matomugo60 <matomugo60@gmail.com>
2022-11-21 14:25:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert);
+  const input = document.getElementById('button');
+input.addEventListener('click', function() {
+alert('I was clicked!');
+});
 
 }
 
-element.addingEventListener('click', clickAlert());
+addingEventListener();
